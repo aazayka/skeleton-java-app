c8b776382cad48f5d2ef3801c543ffa699b54f46
aazayka <aazayka@mail.ru>
2020-06-24 12:06:30 +0300

    5+1

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -14,6 +14,11 @@ public class QueryProcessor {
         if (query.toLowerCase().contains("what is 5 plus 1")) {
             return "6";
         }
+
+        if (query.toLowerCase().contains("what is 14 plus 14")) {
+            return "28";
+        }
+
         if (query.toLowerCase().contains("which of the following numbers is the largest: 96, 44")) {
             return "96";
         }
